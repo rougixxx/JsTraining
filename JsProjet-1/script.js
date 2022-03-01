0e4a7aea7d40e51e859c1744522ac9b131d3dcb0
YacineRx <my.baghdadli@esi-sba.dz>
2022-03-01 19:02:46 +0100

    qsdf

--- JsProjet-1/script.js
+++ JsProjet-1/script.js
@@ -1,50 +1,49 @@
-"use strict";
-
+const btnRoll = document.querySelector(".btn--roll");
+const imgDice = document.querySelector(".dice");
+const current0El = document.querySelector("#current--0");
 
 const player0El = document.querySelector(".player--0");
 const player1El = document.querySelector(".player--1");
 
 
-// ROLL DICE
-let diceImg = document.querySelector(".dice");
-const btnRoll = document.querySelector(".btn--roll");
-const currentScore0El = document.getElementById('current--0');
+console.log(player0El);
+console.log(player1El);
 
-let currentScore0 = 0;
-let currentScore1 = 0;
+let currentScore = 0;
 
-let activePlayer = 0;
+console.log(btnRoll);
+console.log(current0El);
+console.log(imgDice);
 
-
-btnRoll.addEventListener("click", function (e) {
-    let diceNumber = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
-    e.preventDefault();
-    
-    currentScore0 += diceNumber;
-    currentScore0El.textContent = currentScore0;
-    diceImg.setAttribute("src", `dice-${diceNumber}.png`);
+const imgSrc = imgDice.getAttribute("src");
 
 
-    // Switch Player
+
+console.log(imgSrc);
+
+
+btnRoll.addEventListener("click", function() {
+    const diceNumber = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+    // console.log(diceNumber);
+    imgDice.setAttribute('src', `dice-${diceNumber}.png`);
+    // imgSrc = imgDice.setAttribute("src", `dice-${diceNumber}.png`)
+
+    currentScore = Number(current0El.textContent) + diceNumber ;
+
     if (diceNumber == 1) {
-        player0El.classList.toggle('player--active');
-        player1El.classList.toggle('player--active');
-    };
-});     
+        current0El.textContent = 0;
+        if (player0El.classList.contains("player--active")) {
+            player0El.classList.remove("player--active");
+            player1El.classList.add("player--active");
+        } 
+    }
+
+    console.log(currentScore);
+
+    current0El.textContent = currentScore;
+    
+    
 
 
-// HOLD BTN
-const btnHold = document.querySelector(".btn--hold");
-const score0El = document.getElementById("score--0");
-
-let score0 = 0;
-score0El.textContent = "0";
-
-
-btnHold.addEventListener('click', function(e) {
-    e.preventDefault();
-    score0 += Number(currentScore0El.textContent);
-    score0El.textContent = score0;
-    currentScore0 = currentScore0El.textContent = 0;
+    // console.log(diceNumber);
 })
-
