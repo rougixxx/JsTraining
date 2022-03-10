734e3090022c111c4038d3f32eee5194ffb68b6a
YacineRx <my.baghdadli@esi-sba.dz>
2022-03-10 14:31:47 +0100

    MAJ

--- JsProjet-1/script.js
+++ JsProjet-1/script.js
@@ -1,8 +1,17 @@
 /////////////////////////////////////////////
 const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector('.btn--hold');
+
 
 const score1El = document.querySelector("#current--0");
 const score2El = document.querySelector('#current--1');
+
+const globalScore1El = document.querySelector('#score--0');
+const globalScor2El = document.querySelector('#score--1');
+
+console.log(globalScore1El, globalScor2El);
+
+
 const imgDice = document.querySelector(".dice");
 
 const player1El = document.querySelector(".player--0");
@@ -10,6 +19,9 @@ const player2El = document.querySelector(".player--1");
 
 let currentScore = 0;
 
+globalScore1El.textContent = 0;
+globalScor2El.textContent = 0;
+imgDice.setAttribute("src", "")
 
 
 btnRoll.addEventListener("click", function() {
@@ -39,5 +51,32 @@ btnRoll.addEventListener("click", function() {
     }
 }) 
 
+let globalScore1 = 0;
+let globalScore2 = 0;
+
+btnHold.addEventListener("click", function() {
+    if (player1El.classList.contains("player--active")) {
+        // 1. Global Score
+        globalScore1 += currentScore;
+        globalScore1El.textContent = globalScore1
+        // 2. Current Score 
+        currentScore = 0;
+        score1El.textContent = currentScore;
+        // 3. Switch Player
+        player1El.classList.toggle("player--active");
+        player2El.classList.toggle("player--active");
+    } else {
+        // 1. Global Score
+        globalScore2 += currentScore;
+        globalScor2El.textContent = globalScore2
+        // 2. Current Score 
+        currentScore = 0;
+        score2El.textContent = currentScore
+
+        // 3. Switch Player
+        player1El.classList.toggle("player--active");
+        player2El.classList.toggle("player--active");
+    }
+})
 
 
